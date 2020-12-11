4588ed9482ea8b031c475c09f2b37fb3d4b5800e
dellahi <mohamed_abdellahi.el_waghf.etu@univ-lemans.fr>
2020-12-11 11:55:16 +0100

    bug fixes

--- app/src/main/java/com/example/snowtam/View/DisplayInfo.java
+++ app/src/main/java/com/example/snowtam/View/DisplayInfo.java
@@ -76,27 +76,101 @@ public class DisplayInfo extends AppCompatActivity {
             final String newLine = "\n";
             all = all.substring( index );
             int k =1 ;
-            Log.e("test","for" + all);
+            int c = 1;
+            Log.e("testSubstring", all);
             for(String line : all.split(newLine)){
 
                 Log.e("test","for" + line);
-                if(line.contains( "A)" )){
-
+                if(line.contains( "A)" )) {
                     int indB = 0;
-                    indB = line.indexOf("B)");
+                    if (line.contains("B)")) {
+                        indB = line.indexOf("B)");
+                        // On ajoute les champs de "A"
+                        infosArray.add(line.substring(2, (int) indB));
+                        fieldsArray.add(line.substring(0, 2));
+                        decodeinfosArray.add(line.substring(2));
+                        decodefieldsArray.add(line.substring(0, 2));
+                        decodeinfosArray.add(line.substring(2, (int) indB) + "-" + titre);
+                        // On ajoute les champs de "B"
+                        infosArray.add(line.substring((int) indB + 2, (int) line.length()));
+                        fieldsArray.add("B)");
+                        String ChampsB = line.substring((int) indB + 2, (int) line.length());
+                        String jour = "";
+                        String mois = "";
+                        String Heure = "";
+                        String minute = "";
 
-                    // On ajoute les champs de "A"
-                    infosArray.add(line.substring(2, (int)indB));
-                    fieldsArray.add(line.substring(0,2));
+                        jour = ChampsB.substring(2, 4);
+                        mois = ChampsB.substring(0, 2);
+                        Heure = ChampsB.substring(4, 6);
+                        minute = ChampsB.substring(6, 8);
 
-                    decodeinfosArray.add(line.substring(2, (int)indB)+"-"+titre);
-                    decodefieldsArray.add( line.substring(0,2) );
 
+                        switch (mois) {
+                            case "01":
+                                mois = "janvier";
+                                break;
+                            case "02":
+                                mois = "février";
+                                break;
+                            case "03":
+                                mois = "mars";
+                                break;
+                            case "04":
+                                mois = "avril";
+                                break;
+                            case "05":
+                                mois = "mai";
+                                break;
+                            case "06":
+                                mois = "juin";
+                                break;
+                            case "07":
+                                mois = "juillet";
+                                break;
+                            case "08":
+                                mois = "août";
+                                break;
+                            case "09":
+                                mois = "septembre";
+                                break;
+                            case "10":
+                                mois = "octobre";
+                                break;
+                            case "11":
+                                mois = "novembre";
+                                break;
+                            case "12":
+                                mois = "décembre";
+                                break;
+                            default:
+                                mois = "mois";
+                        }
+
+                        decodeinfosArray.add(jour + " " + mois + " " + Heure + "h" + minute + " UTC");
+                        decodefieldsArray.add("B");
+                    }
+
+                   else {
+                       // On ajoute les champs de "A"
+                    infosArray.add(line.substring(2));
+                    fieldsArray.add(line.substring(0, 2));
+
+                    decodeinfosArray.add(line.substring(2));
+                    decodefieldsArray.add(line.substring(0, 2));
+                }
+                }
+
+                else if (line.contains("B)") && c == 1){
+                    c=0;
+                    int indC = 0;
+                    indC = line.indexOf("C)");
+                    int indB = line.indexOf("B)");
                     // On ajoute les champs de "B"
-                    infosArray.add(line.substring((int)indB + 2 , (int)line.length()));
+                    infosArray.add(line.substring((int)indB, (int)indC));
                     fieldsArray.add("B)");
 
-                    String ChampsB = line.substring((int)indB + 2 , (int)line.length());
+                    String ChampsB = line.substring((int)indB, (int)indC);
                     String jour= "";
                     String mois = "";
                     String Heure = "";
@@ -151,9 +225,30 @@ public class DisplayInfo extends AppCompatActivity {
 
                     decodeinfosArray.add( jour+" "+mois+" "+Heure+"h"+minute+" UTC" );
                     decodefieldsArray.add("B");
-                }
 
-                else if(line.contains( "C)" ) &&  k == 1){
+                    String subC  = line.substring( (int)indC, (int)line.length());
+
+                    System.out.println( "SubC ==> " + subC );
+
+                    if(subC.contains("F)")){
+                       int indF = subC.indexOf("F)");
+                      // On ajoute les champs de "C"
+                        infosArray.add(subC.substring(2, (int)indF));
+                        fieldsArray.add(subC.substring(0,2));
+                        decodeinfosArray.add("RUNWAY"+subC.substring(2, (int)indF));
+                        decodefieldsArray.add("C)");
+                        
+                    }
+                    else{// On ajoute les champs de "C"
+                    infosArray.add(line.substring((int)indC+2, (int)line.length()));
+                    fieldsArray.add("C)");
+
+                    decodeinfosArray.add("RUNWAY"+line.substring((int)indC+2, (int)line.length()));
+                    decodefieldsArray.add("C");
+                    }
+
+                }
+                else if(line.contains( "F)" ) &&  k == 1){
                     k=0;
                     int indF = 0;
                     indF = line.indexOf("F)");
@@ -163,11 +258,11 @@ public class DisplayInfo extends AppCompatActivity {
                     System.out.println( "SubF ==> " + subF );
 
                     // On ajoute les champs de "C"
-                    infosArray.add(line.substring(2, (int)indF));
+                    /*infosArray.add(line.substring(2, (int)indF));
                     fieldsArray.add(line.substring(0,2));
 
                     decodeinfosArray.add("RUNWAY"+line.substring(2, (int)indF));
-                    decodefieldsArray.add("C");
+                    decodefieldsArray.add("C");*/
                     if(subF.contains( "G)" )){
 
                         int indG = 0;
@@ -179,6 +274,19 @@ public class DisplayInfo extends AppCompatActivity {
 
                         // On ajoute les champs de "G"
                         infosArray.add(subF.substring((int)indG + 2 , (int)subF.length()));
+                        fieldsArray.add(subF.substring(0,2));
+                    }
+                    else if(subF.contains( "H)" )){
+
+                        int indH = 0;
+                        indH = subF.indexOf("H)");
+
+                        // On ajoute les champs de "F"
+                        infosArray.add(subF.substring(2, (int)indH));
+                        fieldsArray.add(subF.substring(0,2));
+
+                        // On ajoute les champs de "H"
+                        infosArray.add(subF.substring((int)indH + 2 , (int)subF.length()));
                         fieldsArray.add(subF.substring(0,2));
                     }
 
